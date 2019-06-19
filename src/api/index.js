bb968fc7a24f0c770ad1151637dff8de6bd7f7c0
Bolton <bolton.otieno@gmail.com>
2019-06-19 11:38:49 +0300

    change pagination

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ const KEY = '5f96323678d05ff0c4eb264ef184556868e303b32a2db88ecbf15746e6f25e02';
 const URL = `https://api.unsplash.com/photos/`
 
 const fetchImages = async (page) => {
-    const response = await fetch(`${URL}?client_id=${KEY}&per_page=3&page=${page}`);
+    const response = await fetch(`${URL}?client_id=${KEY}&per_page=6&page=${page}`);
     const data = await response.json();
     if (response.status === 400){
         throw new Error(data.errors);
