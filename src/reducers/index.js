726e8a1a9301f08153595d6cc8a978c206183edb
Bolton <bolton.otieno@gmail.com>
2019-06-19 10:55:18 +0300

    add image components

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,14 @@ import { combineReducers } from "redux";
 
 import loadingReducer from "./loadingReducer";
 import imageReducer from "./imagesReducer";
+import pageReducer from "./pageReducer";
+import errorReducer from "./errorReducer";
 
 const rootReducer = combineReducers({
     isLoading: loadingReducer,
     images: imageReducer,
+    nextPage: pageReducer,
+    error: errorReducer,
 })
 
 export default rootReducer;
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,19 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import createSagaMiddleware from "redux-saga";
+import rootReducer from "../reducers";
 import { composeWithDevTools } from "redux-devtools-extension";
+import watchImagesLoad from "../sagas"
 
 const configureStore = () => {
     const sagaMiddleware = createSagaMiddleware()
     const store = createStore(
+        rootReducer,
         compose(
             applyMiddleware(sagaMiddleware),
             composeWithDevTools()
         ),
     );
+    sagaMiddleware.run(watchImagesLoad)
     return store;
 }
 
